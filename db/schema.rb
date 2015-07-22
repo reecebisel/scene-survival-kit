2da76358d77f5004136bb126523455ca0d8dbe35
Tiffany Forsyth <tiffanyforsyth1@gmail.com>
2015-07-22 17:25:31 -0600

    some styling stuff on events pages
    Change-Id: Ife5711343e381b1763073039407a7b62648bddfb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719221656) do
+ActiveRecord::Schema.define(version: 20150721195942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 20150719221656) do
     t.integer  "profile_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string   "title"
+    t.string   "address"
+    t.float    "longitude"
+    t.float    "latitude"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "events", force: :cascade do |t|
