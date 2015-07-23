c09bb3f609ff91a7c5562df96e85093e8e45ccd8
reecebisel <reecebisel@gmail.com>
2015-07-23 17:07:09 -0600

    helper method for a random profile.
    Change-Id: I90df4fc14e0414ae60e243b8d73abfc749e9e962

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,20 @@ module ApplicationHelper
   def find_parent_profile(profile_id)
     Profile.find(profile_id)
   end
+
+  def random_profile
+    Profile.all.sample
+  end
+
+  def profile_type(profile)
+    if Profile.musician.any?
+      'Musician'
+    elsif Profile.venue_rep.any?
+      'Venue Rep.'
+    elsif Profile.visual_artist.any?
+      'Visual Artist'
+    else
+      ' '
+    end
+  end
 end
