d2f3f2be3a0c39f04c6e2480739d059d08cfd21d
Doyi Lee <doyilee@hotmail.com>
2015-07-25 12:21:48 -0600

    commented out current address search, which is broken anyways.
    Change-Id: I173518b1a74580682368489faafbf96397a7b264

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,30 +1,25 @@
 class Search < ActiveRecord::Base
 	def search_profile
-		profiles = Profile.all
-		profile = profiles.where(["name LIKE ? OR bio LIKE ?", "%#{keywords}%", "%#{keywords}%"]) if keywords.present?
+		# keyword
+		profile = Profile.where(["name LIKE ? OR bio LIKE ?", "%#{keywords}%", "%#{keywords}%"]) if keywords.present?
 		
-		#profile by musician attribute
+		# profile by musician attribute
 		profile = Profile.joins(:musician).where(:musicians => { :instrument => "#{instrument}" }) if instrument.present?
 		profile = Profile.joins(:musician).where(:musicians => { :genre => "#{genre}" }) if genre.present?
 
-		#profile by visual artist attribute
+		# profile by visual artist attribute
 		profile = Profile.joins(:visual_artist).where(:visual_artists => { :medium => "#{medium}" }) if medium.present?
 
-		#profile by venue rep attribute
+		# profile by venue rep attribute
 		profile = Profile.joins(:venue_rep).where(:venue_reps => { :venue => "#{venue}" }) if venue.present?
 
 		#find by address
-		profile = Profile.joins(:musician).where(:musicians => { :genre => "#{genre}" }) if genre.present?
-
+		# profile = Profile.joins(:musician).joins(:address).where(:addresses => { :zip => "#{zip}" }) if zip.present?
+		# profile = Profile.joins(:musician).joins(:address).where(:addresses => { :state => "#{state}" }) if state.present?
+		# profile = Profile.joins(:visual_artist).joins(:address).where(:addresses => { :zip => "#{zip}" }) if zip.present?
+		# profile = Profile.joins(:visual_artist).joins(:address).where(:addresses => { :state => "#{state}" }) if state.present?
+		# profile = Profile.joins(:venue_rep).joins(:address).where(:addresses => { :zip => "#{zip}" }) if zip.present?
+		# profile = Profile.joins(:venue_rep).joins(:address).where(:addresses => { :state => "#{state}" }) if state.present?
 		return profile
 	end
-
-# 	def search_address
-# 		musician = Musician.joins(:addresses).where(addresses: {"zip LIKE ?", "%#{zip}%"}) if zip.present?
-		# musician = Musician.joins(:addresses).where(addresses: {"state LIKE ?", "%#{state}%"}) if zip.present?
-		# venue_rep = VenueRep.joins(:addresses).where(addresses: {"zip LIKE ?", "%#{zip}%"}) if zip.present?
-		# venue_rep = VenueRep.joins(:addresses).where(addresses: {"state LIKE ?", "%#{state}%"}) if zip.present?
-		# visual_artist = VisualArtist.joins(:addresses).where(addresses: {"zip LIKE ?", "%#{zip}%"}) if zip.present?
-		# visual_artist = VisualArtist.joins(:addresses).where(addresses: {"state LIKE ?", "%#{state}%"}) if zip.present?
-	# end
 end 
