254e3ca08e19ee0736bde8c8456b0f77d47cb886
Reece <reecebisel@users.noreply.github.com>
2015-07-26 13:14:21 -0600

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :twitter, ENV['1zWRQZ2GpHUstPfsmRErLkSWD'], ENV['UEAYZucVTN9a8USoyQr6DeABCTBgSSj4X0t7CoaNsP6PJkH0jV']
-	provider :facebook, ENV['1013478302035989'], ['fbcf933ffb14418c2b6534b90af92c0d']
+	provider :twitter, ENV['secret stuff for twitter']
+	provider :facebook, ENV['secret stuff for facebook']
 end
